c7521977d66d08c98af1748429b405be506636b5
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-05-11 13:55:36 -0700

    routing and linkUrl, lesson 69

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import HomePage from './pages/homepage.component';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div >
-     <HomePage />
+      <Switch>
+      <Route path='/shop/hats'>
+          <HatsPage />
+        </Route>
+        <Route path='/'>
+          <HomePage />
+        </Route>
+     </Switch>
     </div>
   );
 }
