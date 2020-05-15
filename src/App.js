3714100b336937cbdb142a18861bd9460e83ff7c
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-05-14 21:03:38 -0700

    add header comp with links to shop and contact. Finished lesson 73

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import { Route, Switch, Link } from 'react-router-dom';
 import HomePage from './pages/homepage.component';
 import ShopPage from './pages/shop/shop.component.js';
-
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
       <Route path='/shop'>
           <ShopPage />
